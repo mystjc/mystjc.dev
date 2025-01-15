0b5a46aae72f3c3af89ac4eba24afce0b470218f
Mystjc <77810795+mystjc@users.noreply.github.com>
2025-01-14 22:47:43 -0500

    Update fetching repos code

--- src/components/githubProjects.svelte
+++ src/components/githubProjects.svelte
@@ -1,28 +1,50 @@
 <script>
   import { onMount } from 'svelte';
-  let repos = null;
-
-  onMount(async () => {
-    const response = await fetch('https://api.github.com/users/mystjc/repos');
-    const data = await response.json();
-    repos = data;
+  
+  let repos = [];
+  let hasFetchedRepos = false;
+  const apiLinks = [
+    "https://api.github.com/repos/mystjc/dotfiles",
+    "https://api.github.com/repos/mystjc/nixfiles",
+    "https://api.github.com/repos/mystjc/zirconia",
+    "https://api.github.com/repos/mystjc/mystjc.dev",
+    "https://api.github.com/repos/mystjc/cobaltic"
+  ]
+  
+  onMount (async () => {
+    await getRepos();
   });
+
+  async function getRepos() {
+    for (let link of apiLinks) {
+      const data = await fetch(link).then(
+        (response) => response.json()
+      );
+      repos.push(data);
+    }
+    hasFetchedRepos = true;
+  }
 </script>
 
-{#if repos}
-  <ul class="text-white">
-    {#each repos as repo}
-      <li>
-        <a href={repo.html_url} target="_blank">View on GitHub</a>
-        <img src="{repo.owner.avatar_url}" alt="" width="10" height="10"> 
-        <p>User: {repo.owner.login}</p>
-        <p>Title: {repo.name}</p>
-        <p>Description: {repo.description}</p>
-        <p>Language: {repo.language}</p>
-        <p>Stars: {repo.stargazers_count}</p>
-      </li>
-    {/each}
-  </ul>
+{#if hasFetchedRepos}
+  {#each repos as repo}
+    <div class="bg-gray-500 rounded-md p-4 m-4">
+      <a href={repo.html_url} target="_blank">
+        <div class="flex items-center">
+          <img class="rounded-full w-6" src={repo.owner.avatar_url.toString()} alt="">
+          <span class="pl-2">{repo.owner.login} / <strong>{repo.name}</strong></span>
+        </div>
+        <span>{repo.description}</span>
+        <div class="flex items-center">
+          {#if repo.language != null}
+            <span class="pr-2">{repo.language}</span>
+          {/if}
+          <img class="w-4" src="../../assets/icons/star-regular.svg" alt="">
+          <span class="pl-1">{repo.stargazers_count}</span>
+        </div>
+      </a>
+    </div>
+  {/each}
 {:else}
   <p>Loading repos...</p>
 {/if}
